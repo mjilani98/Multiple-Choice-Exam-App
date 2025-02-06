691e9339d13122a5c86886c728d999ecb9a562d5
mjilani98 <mjilani@emich.edu>
2025-02-06 15:48:01 -0500

    Created an intialize method in the main class (controller) Added a next button in user interface (view)

--- app/src/main/java/com/example/multiplechoiceexamapp/MainActivity.java
+++ app/src/main/java/com/example/multiplechoiceexamapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.multiplechoiceexamapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+
+    private QuestionsGenerator generator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,58 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        //Create a generator
+        generator = new QuestionsGenerator();
+
+        //Initialize
+        initialize(generator);
+
+
+
     }
+
+    public void initialize(QuestionsGenerator generator)
+    {
+        //- generate
+        generator.generate();
+
+        //- get question, four answers
+        String question = generator.getQuestions();
+        String answer1 = generator.getAnswer1();
+        String answer2 = generator.getAnswer2();
+        String answer3 = generator.getAnswer3();
+        String answer4 = generator.getAnswer4();
+
+        //- display question, four answers in radio
+
+        //Getting hold on the question text view and display the question
+        TextView questionOutPut = findViewById(R.id.txtViewQuestion);
+        questionOutPut.setText(question);
+
+        //getting hold of the radio button to display the answer
+        RadioButton answer1Btn = findViewById(R.id.answer1);
+        RadioButton answer2Btn = findViewById(R.id.answer2);
+        RadioButton answer3Btn = findViewById(R.id.answer3);
+        RadioButton answer4Btn = findViewById(R.id.answer4);
+
+        //displaying the answers to the radio buttons
+        answer1Btn.setText(answer1);
+        answer2Btn.setText(answer2);
+        answer3Btn.setText(answer3);
+        answer4Btn.setText(answer4);
+
+        //- display 0/0
+        TextView scoreView = findViewById(R.id.txtViewResult);
+        scoreView.setText("0/0");
+
+        //- enable submit button, disable next button
+        Button btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit.setEnabled(true);
+
+        Button btnNext = findViewById(R.id.btnNext);
+        btnNext.setEnabled(false);
+
+    }
+
+
 }
