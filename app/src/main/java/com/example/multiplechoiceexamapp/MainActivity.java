876c2c3a62ffcd2be17cd5609aef25a2908fc929
mjilani98 <mjilani@emich.edu>
2025-02-06 20:02:50 -0500

    Added a working next button (controller)

--- app/src/main/java/com/example/multiplechoiceexamapp/MainActivity.java
+++ app/src/main/java/com/example/multiplechoiceexamapp/MainActivity.java
@@ -121,14 +121,94 @@ public class MainActivity extends AppCompatActivity {
             TextView resultView = findViewById(R.id.txtViewResult);
             resultView.setText(correct+"/"+total);
 
+            //get the index of the current question to know if it arrives to the last question
+            int index = generator.getIndex();
+
+            //the size is the number of questions available
+            int size = generator.getSize();
+
             //- enable submit button, disable next button
             Button btnSubmit = findViewById(R.id.btnSubmit);
             btnSubmit.setEnabled(false);
 
             //disable next button
-            Button btnNext = findViewById(R.id.btnNext);
-            btnNext.setEnabled(true);
+            if(index != size-1)
+            {
+                Button btnNext = findViewById(R.id.btnNext);
+                btnNext.setEnabled(true);
+            }
+
         }
+
+
+    }
+
+    public void next(View v)
+    {
+        //- generate
+        generator.generate();
+        //- get question, four answers
+        String question = generator.getQuestions();
+        String answer1 = generator.getAnswer1();
+        String answer2 = generator.getAnswer2();
+        String answer3 = generator.getAnswer3();
+        String answer4 = generator.getAnswer4();
+
+        //- display question, four answers in radio
+        //Getting hold on the question text view and display the question
+        TextView questionOutPut = findViewById(R.id.txtViewQuestion);
+        questionOutPut.setText(question);
+
+        //getting hold of the radio button to display the answer
+        RadioButton answer1Btn = findViewById(R.id.answer1);
+        RadioButton answer2Btn = findViewById(R.id.answer2);
+        RadioButton answer3Btn = findViewById(R.id.answer3);
+        RadioButton answer4Btn = findViewById(R.id.answer4);
+
+        //displaying the answers to the radio buttons
+        answer1Btn.setText(answer1);
+        answer2Btn.setText(answer2);
+        answer3Btn.setText(answer3);
+        answer4Btn.setText(answer4);
+        //- uncheck all buttons
+        answer1Btn.setChecked(false);
+        answer2Btn.setChecked(false);
+        answer3Btn.setChecked(false);
+        answer4Btn.setChecked(false);
+
+        //- get correct, total
+        int correct = generator.getCorrect();
+        int total = generator.getTotal();
+
+
+
+
+
+            //- display correct/total
+            TextView resultView = findViewById(R.id.txtViewResult);
+            resultView.setText(correct+"/"+total);
+
+            //- enable submit button, disable next button
+            Button btnSubmit = findViewById(R.id.btnSubmit);
+            btnSubmit.setEnabled(true);
+
+            //disable next button
+            Button btnNext = findViewById(R.id.btnNext);
+            btnNext.setEnabled(false);
+
+
+
+        //- deal with case where there are no more questions
+        //get the index of the current question to know if it arrives to the last question
+        int index = generator.getIndex();
+
+        //the size is the number of questions available
+        int size = generator.getSize();
+
+
+
+
+
 
 
     }
